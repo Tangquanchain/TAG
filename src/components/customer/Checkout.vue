48463da5a0041ed5a7055e17e8ddda3ab344d41e
Tom <Tang@gmail.com>
2019-11-12 22:52:28 +0800

    first 11/12

---
+++ src/components/customer/Checkout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="wrap">
+      <Aside />
+      <Navbar />
+      <router-view />
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar";
+import Aside from "./Aside";
+import Footer from "./Footer";
+import $ from "jquery";
+export default {
+  components: {
+    Navbar,
+    Aside,
+    Footer
+  },
+};
+</script>
+
+<style lang="scss">
+.wrap {
+  transition: transform 0.3s;
+  position: relative;
+}
+
+.wrap.active {
+  transform: translateX(450px);
+}
+</style>
